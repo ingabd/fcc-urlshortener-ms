94f5ef1dd522094cf2da6a84d07bcb2357dff41b
Abdulrachman Hasan <hasanabdulrachman@gmail.com>
2022-02-15 19:36:39 +0700

    Add first feature /api/shorturl and db.json

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const fs = require('fs')
+
+const pathToDb = './db.json'
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +25,29 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', (req, res) => {
+  const { original_url } = req.body
+  
+  const db = JSON.parse(fs.readFileSync(pathToDb, 'utf8'))
+  
+  const lastId = db[db.length - 1].short_url
+  const newId = +lastId + 1
+
+  const newItem = {
+    original_url,
+    short_url: newId
+  }
+
+  db.push(newItem)
+  fs.writeFileSync(pathToDb, JSON.stringify(db, null, 2))
+
+  res.json(newItem)
+})
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+  
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
